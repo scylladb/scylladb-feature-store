a734970a66290a4f7001526875498886123485dc
Attila Toth <hello@attilatoth.dev>
2025-06-10 18:39:49 +0200

    add used cars example

---
+++ used_cars/config.py
@@ -0,0 +1,9 @@
+SCYLLA_HOSTS = ["172.18.0.4"]
+SCYLLA_KEYSPACE = 'feature_store'
+SCYLLA_USER = 'scylla'
+SCYLLA_PASS = ''
+SCYLLA_DC = 'datacenter1'
+
+SCHEMA_FILE = 'schema.cql'
+TABLE = 'car_features'
+CSV_FOLDER = './data'
